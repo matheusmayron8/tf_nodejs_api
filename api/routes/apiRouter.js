d28fe60f4b6f65166be13ffdd278fcd2cd8d0151
Matheus Lima <matheus.lima@gerencianet.com.br>
2022-11-20 20:57:12 -0300

    #Fix: Fixed validate body in PUT. Wrong validation.

--- api/routes/apiRouter.js
+++ api/routes/apiRouter.js
@@ -86,7 +86,7 @@ apiRouter.post(endpoint + 'notes', checkToken, (req, res) => {
 apiRouter.put(endpoint + 'notes/:id', checkToken, (req, res) => {
     let id = req.params.id
     console.log(req.body)
-    if (req.body.completed != undefined) {
+    if (req.body.completed == undefined) {
         res.status(400).json({message: 'Campo [completed] é obrigatório.'})
         return
     }
